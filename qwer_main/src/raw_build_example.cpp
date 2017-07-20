8d34f9abd1db8f10c3e831b50fb26e73995af12c
DeathPoem <wangzhihongjisina@gmail.com>
2017-07-20 14:58:19 +0800

    update

--- qwer_main/src/raw_build_example.cpp
+++
@@ -1,7 +0,0 @@
-#include "./lib/facility.h"
-
-int main() {
-    my_http::Logger::get_logger().repare("").log("log something in this example");
-    //return my_http::MyFacilityTest();
-    return 0;
-}
